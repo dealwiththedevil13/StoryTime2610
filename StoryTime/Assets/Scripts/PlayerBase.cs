516de59b0c3c4cccec0d63290b74a162ec975f3a
Amanda <10932525@uvu.edu>
2023-01-21 23:56:04 -0700

    Added first material and started PlayerAbstract

---
+++ StoryTime/Assets/Scripts/PlayerBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerBase : MonoBehaviour
+{
+    public abstract void Start();
+   
+
+    
+}
