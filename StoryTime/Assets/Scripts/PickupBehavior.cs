52182ac453a53c54bdc888b0d2df8a90889da86a
Amanda <10932525@uvu.edu>
2023-11-16 01:53:39 -0700

    pickup script pt.2 and added audio

--- StoryTime/Assets/Scripts/PickupBehavior.cs
+++ StoryTime/Assets/Scripts/PickupBehavior.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PickupBehavior : MonoBehaviour
 {
-   public ID pickupID;
-
-   public void OnTriggerEnter(Collider other)
-   {
-      if (other.TryGetComponent(out CarryBehavior carry))
+  public ID pickupID;
+  
+    public UnityEvent onPickup;
+  
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.TryGetComponent(out Carry carry))
       {
-         carry.PickUp(this);
+        if(carry.placedID == pickupID)
+        {
+          carry.PickUp(this);
+          onPickup.Invoke();
+        }
       }
-   }
-}
+    }
+  }
+
+  public class Carry : MonoBehaviour
+  {
+    public ID placedID;
+  
+    public void PickUp(PickupBehavior pickup)
+    {
+      // Pick up logic
+    }
+  }
